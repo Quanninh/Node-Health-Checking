2e07d538f9f654fa6aed670a3cd99a0849f3f9fd
Quanninh <quanninh05@gmail.com>
2026-04-22 14:32:51 +0700

    Implement Node.java

--- ServerApplication/src/main/java/com/monitoring/model/Node.java
+++ ServerApplication/src/main/java/com/monitoring/model/Node.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
-
 public class Node {
     @Id
     private String id;
@@ -14,7 +13,55 @@ public class Node {
     private double memoryUsage;
     private String ipAddress;
 
+    private LocalDateTime lastHeartbeat;
+
+    private String status;
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public double getCpuUsage(){
+        return cpuUsage;
+    }
+
+    public void setCpuUsage(double cpuUsage){
+        this.cpuUsage = cpuUsage;
+    }
+
+    public double getMemoryUsage(){
+        return memoryUsage;
+    }
+
+    public void setMemoryUsage(double memoryUsage){
+        this.memoryUsage = memoryUsage;
+    }
+
     public String getIpAddress(){
         return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress){
+        this.ipAddress = ipAddress;
+    }
+
+    public LocalDateTime getLastHeartbeat(){
+        return lastHeartbeat;
+    }
+
+    public void setLastHeartbeat(LocalDateTime lastHeartbeat){
+        this.lastHeartbeat = lastHeartbeat;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
     }
 }
