b16c69fa7944a1be90cc79c6b847096d2cbad2ec
tohuynhphuc <140296933+tohuynhphuc@users.noreply.github.com>
2026-05-24 19:51:13 +0700

    Add DiscoveryMessage dataflow

--- node-agent/src/main/java/com/monitoring/agent/node/connection/DiscoveryMessageType.java
+++ node-agent/src/main/java/com/monitoring/agent/node/connection/DiscoveryMessageType.java
@@ -11,12 +11,18 @@ public enum DiscoveryMessageType {
      * adds the joining node as a neighbor.
      */
     COMMIT_VICTIM,
-    /** */
-    COMMIT_ACK,
     /** The direct target node adds the joining node as a neighbor. */
     COMMIT_SMALL_JOIN,
-    /** */
+    /** Acknowledges the commit (accepted or rejected) */
+    COMMIT_ACK,
+    /**
+     * The joining node send requests (broadcast) to all nodes, asking to join the
+     * network.
+     */
     JOIN_REQUEST,
-    /** */
+    /**
+     * Nodes that are in the network accepts the join request, but not confirm yet
+     * because the join request is a broadcast message.
+     */
     JOIN_ACK
 }
