158e000ed0fd98d93123bd076c35302032b82cbd
Quanninh <quanninh05@gmail.com>
2026-05-24 21:51:02 +0700

    append to log.txt

--- node-agent/src/main/java/com/monitoring/agent/util/Console.java
+++ node-agent/src/main/java/com/monitoring/agent/util/Console.java
@@ -1,30 +1,56 @@
 package com.monitoring.agent.util;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import com.monitoring.agent.constant.Constant;
 
 public class Console {
 
     private static String nodeId;
 
+    private static final String LOG_FILE = "log.txt";
+
+    private static synchronized void writeToFile(String text) {
+        try (FileWriter fw = new FileWriter(LOG_FILE, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+
+            out.println(text);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void setNodeId(String nodeId) {
         // not working
-        this.nodeId = nodeId;
+        Console.nodeId = nodeId;
+        writeToFile(nodeId);
     }
 
     public static void log(String message, String color) {
-        System.out.println("[" + Constant.NOW() + "] " + color + message + Constant.RESET);
+        String lineConsole = "[" + Constant.NOW() + "] " + color + message + Constant.RESET;
+        String line = "[" + Constant.NOW() + "] " + message + Constant.RESET;
+        System.out.println(lineConsole);
+        writeToFile(line);
     }
 
     public static void log(String message) {
         System.out.println("[" + Constant.NOW() + "] " + message);
+        writeToFile("[" + Constant.NOW() + "] " + message);
     }
 
     public static void println(String message) {
         System.out.println(message);
+        writeToFile(message);
     }
 
     public static void println(String message, String color) {
         System.out.println(color + message + Constant.RESET);
+        writeToFile(message);
     }
 
 }
