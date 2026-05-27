e8e41acfb9b33e3c6b2c44a3c72f333041176258
NguyenHaKhai <nguyenhakhai2005@gmail.com>
2026-05-27 15:13:17 +0700

    update javadoc

--- node-agent/src/main/java/com/monitoring/agent/node/recovery/RecoveryMessageType.java
+++ node-agent/src/main/java/com/monitoring/agent/node/recovery/RecoveryMessageType.java
@@ -8,16 +8,33 @@ public enum RecoveryMessageType {
 
     /** Requests a direct rewire. */
     REWIRE_REQ_DIRECT,
-    /** */
+    /** 
+     * The receiver sends back to the sender of REWIRE_REQ_DIRECT in case of success.
+     * Once that sender receives this, it adds the receiver node to its neighbor list
+     * and send REWIRE_DIRECT_COMMIT to the receiver.
+     */
     REWIRE_REQ_DIRECT_ACK,
-    /** */
+    /**
+     * Message sent to the node that receives REWIRE_REQ_DIRECT. Once it received this message,
+     * it updates its neighbor list if successful.
+     */
     REWIRE_DIRECT_COMMIT,
 
-    /** */
+    /**
+     * Sent to ask other deficient node if it wants to join the recovery process.
+     * If the receiver CANNOT become DEFICIENT_FELLOW, it refuses.
+     */
     REWIRE_SESSION_REQ,
-    /** */
+    /**
+     * If the receiver of REWIRE_SESSION_REQ can become DEFICIENT_FELLOW, it 
+     * send back this message to sender. The sender then will become the DEFICIENT_LEADER
+     * and sends REWIRE_SESSION_COMMIT.
+     */
     REWIRE_SESSION_ACK,
-    /** */
+    /**
+     * Message sent to the node that receives REWIRE_SESSION_REQ. Once it received this message,
+     * it becomes DEFICIENT_FELLOW.
+     */
     REWIRE_SESSION_COMMIT,
 
     /** Requests a full rewire. */
